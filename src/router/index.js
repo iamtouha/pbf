d9dcda2720c1607e5f437bcf5849c4116a6a053f
Touha Zohair <touha98@gmail.com>
2021-10-11 13:59:53 +0600

    navigation guard removed

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-import { auth } from "../firebase";
-import store from "../store";
 import Homepage from "../pages/Home.vue";
 
 const router = createRouter({
@@ -8,28 +6,15 @@ const router = createRouter({
   routes: [
     {
       path: "/",
+      name: "home",
       component: Homepage,
     },
     {
       path: "/form",
+      name: "form",
       component: () => import("../pages/Form.vue"),
-      meta: {
-        auth: true,
-      },
     },
   ],
-});
-
-router.beforeEach((to, from, next) => {
-  const user = auth.currentUser;
-  if (to.meta.auth && user) {
-    return next();
-  }
-  if (to.meta.auth && !user) {
-    store.commit("REGISTER_DIALOG_OPEN");
-    return next("/");
-  }
-  return next();
 });
 
 export default router;
